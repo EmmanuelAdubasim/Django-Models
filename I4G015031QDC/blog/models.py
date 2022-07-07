23aaee711b63048041447db5696c31fa18c0b085
EmmanuelAdubasim <adubasimemmanuelikechukwu01@gmail.com>
2022-07-07 15:09:54 +0100

    first commit

---
+++ I4G015031QDC/blog/models.py
@@ -0,0 +1,31 @@
+from cgitb import text
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.urls import reverse
+# from django.template.defaultfilters import slugify
+
+# Create your models here.
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published")
+    )
+
+    # DB Fields
+    title = models.CharField(max_length=200)
+    # slug = models.SlugField(max_length=300, unique=True, editable=False)
+    text = models.TextField()
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    body = models.TextField()
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="draft"
+    )
